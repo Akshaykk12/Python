a9003eda5bbd188f747725e8926a23f622bab27a
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2024-09-12 16:33:10 +0530

    pdf resume works  well

--- AI projects/Resume Bot/app.py
+++ AI projects/Resume Bot/app.py
@@ -3,10 +3,30 @@ import streamlit as st
 from templates import resumeTemplate
 from uprofile import myProfile
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
+# import easyocr
+# import numpy as np
+# from PIL import Image
+# from pdf2image import convert_from_bytes
+from pypdf import PdfReader
+
 
 genai.configure(api_key=os.getenv("API_KEY"))
+# reader = easyocr.Reader(["en"])
 
-def create_prompt(job_description):
+def create_prompt(job_description, profile):
+
+    upgraded_prompt = f"""
+    You are an expert latex resume writer with over 20 years of experienceworking with job seekers trying to land a role in tech.
+    Highlight the 3 most important responsibilities in this job description:
+    {job_description}
+    Based on these 3 most important responsibiities from job description, please tailor a Latex resume for this role. Do not make information up. Here's my resume:
+    {profile}
+    Please use this latex resume for reference:
+    {resumeTemplate}
+    And accordingly give me code of that Latex resume. It should be one page Latex resume, so arrange elements horizontally wherever required to save up space.
+    Don't give any explaination at the end, just a latex code. Based on these 3 most important responsibiities from job description, please tailor a Latex resume for this role.
+    """
     
     prompt = f"""
     I would like to generate a custom professional resume .
@@ -22,28 +42,54 @@ def create_prompt(job_description):
     - Ensure the resume is formatted professionally.
     - Structure the resume to include sections like: Personal Information, Objective, Experience, Skills, Education, and Certifications.
     - Align the content from my profile with the job description, making it relevant and specific to the role.
-    - Ensure the  properly formatted and includes any necessary packages or formatting settings.
+    -    Ensure the  properly formatted and includes any necessary packages or formatting settings.
 
     
 
-    Please generate the resume using this template only. Mimic every little details. 
+    Please generate the resume using this template only. Mimic every little details. You can order the arrangement of the sections such that it matches with the job. Make sure that the Latex code is sufficient for one page resume.
+    Make sure you use "textbf" for bolding the texts. And use margin of only 2cm.
+    Give me Latex code such that Name(At top of resume Big and bold) and contact details will be at the top of the resume. Contact information will be  organized horizontally. Dont add any fake information like work experience and dont write objective but instead use summary there.
     
     """
 
 
-    return prompt
+    return upgraded_prompt
 
 def generate_latex_resume(prompt):
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(prompt, generation_config= genai.types.GenerationConfig(temperature=0.0))
-    return response
+    response = model.generate_content(prompt, safety_settings={
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    })
+    return response.text
 
+# def perform_ocr(image):
+#     image_np = np.array(image)
+#     results = reader.readtext(image_np)
+#     text = " ".join(result[1] for result in results)
+#     return text
+
+def read_pdf(uploaded_file):
+    reader = PdfReader(uploaded_file)
+    page = reader.pages[0]
+
+    text = page.extract_text()
+    return text
+#     pdf = PDFQuery(uploaded_file)
+#     pdf.load()
+#     text_elements = pdf.pq("LTTextLineHorizontal")
+#     text = [t.text for t in text_elements]
+#     return text
 
 
 st.title("Resume Bot")
 job_description = st.text_area("Enter the job description", height = 260)
+uploaded_file = st.file_uploader("Upload your Resume", type=["pdf"])
 
-if job_description:
-    prompt = create_prompt(job_description)
+if st.button("Generate Resume"):
+
+    text = read_pdf(uploaded_file)
+    prompt = create_prompt(job_description, text)
     latex_resume = generate_latex_resume(prompt)
     st.code(latex_resume)
+        
--- AI projects/Resume Bot/uprofile.py
+++ AI projects/Resume Bot/uprofile.py
@@ -1,58 +1,81 @@
 myProfile= """
-    Name= Akshay Kudalkar
-    Location= Pune, Maharashtra, India 
-    Email id= akshaykudalkar.12@gmail.com
-    Mobile= +917028327664 
-    Website= https://akshaykk12.vercel.app
-    Linkedin= linkedin.com/in/akshay-kudalkar 
-    Github= github.com/Akshaykk12
-
-    Summary=
-    I am a passionate AI/ML Developer and Blockchain Enthusiast with a robust foundation in Machine Learning and
-    Data Science. I excel in creating advanced ML models and have developed expertise in blockchain technologies,
-    including Hyperledger Fabric. I am proficient in web/app development technologies like HTML/CSS, JavaScript,
-    React.js, VueJS, Tailwind CSS, and Flutter.
-
-    Education=
-    1)Dr. D. Y. Patil Institute of Technology, BTech in Artificial Intelligence and Data
-    Science, GPA: 8.3
-    2021- Present
-    • Coursework: Computer Architecture, Artificial Intelligence, Comparison of Learning Algorithms, Computational
-    Theory
-    2)Sangmeshwar College, 12th MSBSHSE, Percentage: 91.67 2019- 2021
-    • Coursework: Statistics, Algebra, Quantum Physics, etc.
-
-    Projects=
-    1)Avalanche Watch
-    • AvalancheWatch is an unofficial implementation of the research paper "Synthetic Aperture Radar (SAR)
-    Monitoring of Avalanche Activity: An Automated Detection Scheme" by H. Vickers, M. Eckerstorfer, E. Malnes,
-    and A. Doulgeris.
-    • This project aims to replicate the results of the paper, demonstrating an advanced algorithm for detecting snow
-    avalanches using SAR technology, which operates effectively regardless of light and weather conditions.
-
-    2)Dental App
-    • Integrated Voice to Text transcription into the app using python programming language.
-    • Conducted thorough user research by interviewing dentists and administrators to understand their workflow
-    and data management challenges. Collected feedback on their needs and preferences to ensure the app met
-    their specific requirements.
-
-    3)Face Swapper App
-    • The Face Swapper project is a fun and creative application that takes two images as input and produces a single
-    output image.
-    • The goal of the project is to swap the faces of the individuals in the input images, creating a humorous and
-    entertaining result.
-
-    Worked on total of 10+ projects
-
-    Key Expertise=
-    • Machine Learning- Computer Vision, NLP, LLM.
-    • Blockchain- Solidity Smart Contract, Hyperledger Fabric.
-    • Web Development- Reactjs, Tailwindcss, Vuejs.
-    • App Development- Flutter.
-    • Programming Language- Python.
-
-    Additional Experience And Awards=
-    1) UI/UX workshop: Taught UI/UX with practical implementation.
-    2) First Runner-up in UI/UX Hackathon: Awarded 2rd prize for the coffee app design
+    SHIRISH HANMANT HIRAPURE
+B.E. - Computer Engineering
+Ph: +91-8767459193
+Email: shirishhirapure1555@gmail.com
+Pune, Maharashtra, India - 411018
+LinkedIn: https://www.linkedin.com/in/shirish-hirapure-533591229/
+Java Data Structures OOPs Web Development Javascript NodeJS Express.js Oracle MySQL MongoDB
+BRIEF SUMMARY
+I have a deep passion for problem-solving, web development, and the intricate world of financial markets, My educational background
+and skills align with these interests, allowing me to dive into these areas with enthusiasm.
+What truly excites me is the prospect of connecting with brilliant minds who share my drive for innovation and creativity. Collaborating
+with such individuals fuels my desire to explore new horizons and continuously expand my knowledge in these dynamic fields
+KEY EXPERTISE
+EDUCATION
+Dr. D. Y. Patil Institute of Technology 2021  -  2025
+B.E. - Computer Engineering | CGPA: 9.18 / 10
+Shri Sangameshwar College, Solapur 2021
+12th | MSBSHSE | Percentage: 97.67 / 100
+V. M. Mehta High School, Solapur 2019
+10th | MSBSHSE | Percentage: 92.60 / 100
+AWARDS AND SCHOLARSHIPS
+I was honored to receive the prestigious award for achieving a perfect 10 CGPA in my first year of college.
+INTERNSHIPS
+Virtual Intern 11 Dec, 2023  - 12 Jan, 2024 
+FullStack Web Development
+FullStack Web Development at Virtual Intern
+PROJECTS
+Secura_CSSD 15 Dec, 2023  -  10 Apr, 2024
+Team Size: 2
+Key Skills: Node.js HTML5 CSS Express.js MongoDB Python
+Project Link:  https://github.com/Siri1555/Secura-CSSD.git
+Secura_CSSD is a security software system designed to provide robust protection for highly confidential data and prevent data
+breaches. It integrates a website and a desktop application, utilizing cyber tools to secure wireless device connections, including Wi-Fi
+and wireless cameras.
+Finance Expense Tracker 09 Jan, 2024  -  22 Feb, 2024
+Team Size: 1
+Key Skills: HTML5 Javascript CSS MongoDB API
+Project Link:  https://github.com/Siri1555/Finance-Expense-Tracker.git
+The Finance Expense Tracker is more than just a web application; it's a dynamic solution meticulously crafted to empower individuals to
+take charge of their financial well-being. It provides an efficient solution for tracking and managing all financial transactions, offering
+real-time suggestions and improvements on individual expenditures through API integration.
+DPU Classroom 11 May, 2023  -  20 Jul, 2023
+Team Size: 3
+Key Skills: Android Studio Kotlin Java
+DPUClassroom, crafted with Python and Android Studio, is a dynamic educational platform akin to Google Classroom. Seamlessly
+blending intuitive design with robust functionality, it offers a streamlined experience for educators and learners, fostering collaboration
+Gender: Male
+Marital Status: Single
+Current Address: 99/13 Sant Tukaram Nagar Pimpri Pune,
+Pune, Maharashtra, India - 411018
+Date of Birth: 12 Nov, 2003
+Permanent Address: At/post Yatnal, South Solapur, Yetnal,
+Solapur, Solapur, Maharashtra, India - 413215
+Phone Number: +91-8767459193
+Email: shirishhirapure1555@gmail.com
+and knowledge exchange.
+ACHIEVEMENTS
+I secured the position of First Runner-Up in my college's PBL annual exhibition
+ASSESSMENTS / CERTIFICATIONS
+The Complete JAVA Course:From Basics To Advanced
+Key Skills: Java OOPs Data Structures
+MERN Stack With Blog Project
+Key Skills: MongoDB Express.js React.js Node.js
+POSITIONS OF RESPONSIBILITY
+Representative at ACES
+The Association of Computer Engineering Students (ACES) at Dr. D. Y. Patil Institute of Technology, Pimpri, is a vibrant club
+fostering collaboration and innovation among passionate students, faculty mentors, and industry enthusiasts.
+ACES strives to create a supportive and collaborative community for holistic development in computer engineering.
+SEMINARS / TRAININGS / WORKSHOPS
+Data Visualization Tools: PowerBI and Tableau 08 Feb, 2024  -  09 Feb, 2024
+Institute Name: DR. D. Y. PATIL UNITECH SOCIETY DR. D. Y. PATIL INSTITUTE OF TECHNOLOGY, PIMPRI
+Key Skills: Tableau Power BI
+PERSONAL INTERESTS / HOBBIES
+Swimming
+Financial and Derivatives Markets Analysis and Trading
+WEB LINKS
+Github - https://github.com/Siri1555
+PERSONAL DETAILS
 
     """
