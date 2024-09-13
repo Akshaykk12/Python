4f76d76ed06ceb2c4dc39e879895770345efcc23
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2024-09-13 23:55:24 +0530

    Linked List tasks added

--- DSA/Day 2(Linked List)/linkedList.py
+++ DSA/Day 2(Linked List)/linkedList.py
@@ -1,3 +1,23 @@
+"""
+What is Linked List?
+Node [D]
+Create LL
+len
+insert from head
+traverse/ print
+insert from tail(append)
+insert in middle(after)
+
+clear
+delete from head
+delete from tail(pop)
+delete by value (remove)
+
+search by value(find)
+delete by index -> del[0]
+search by index (indexing)
+
+"""
 
 class Node:
 
@@ -6,3 +26,15 @@ class Node:
         self.next = None
 
 a = Node(1)
+b = Node(34)
+c = Node(29)
+print(a.value)
+print(id(a))
+print(id(b))
+print(id(c))
+
+a.next = b
+b.next = c                  #here we formed linkedlist manually
+print(a.next)
+print(b.next)
+print(c.next)
